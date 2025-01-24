d762eb934f4ee080e8fefc84b74757e40d21bdcd
gamja-12 <jyh7444@naver.com>
2025-01-24 13:13:26 +0900

    Add files via upload

---
+++ 0124/ex05.js
@@ -0,0 +1,13 @@
+const a = 65;
+const b = 100;
+
+console.log("a===b는 ?");
+console.log(a===b);
+
+console.log(10 / 0);
+console.log(2 * 'String');
+
+const c = 10;
+const C = 20;
+console.log(c);
+console.log(C);
